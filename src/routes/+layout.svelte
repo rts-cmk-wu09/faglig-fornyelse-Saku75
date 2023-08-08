616072138117644ecb1b71c2b75c8e2aefa39ef7
Lukas Vendel Mann <lukas@lvmann.io>
2023-08-08 15:28:26 +0200

    Setup fullscreen function

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
   import "../app.scss";
+  import { appWindow } from "@tauri-apps/api/window";
+
+  async function toggleFullscreen() {
+    appWindow.setFullscreen(!(await appWindow.isFullscreen()));
+  }
 </script>
+
+<button
+  class="fixed right-4 top-4 z-50 rounded-md bg-neutral-700 p-2 opacity-25 transition-opacity hover:opacity-75"
+  on:click={toggleFullscreen}>Fullscreen</button
+>
 
 <slot />
