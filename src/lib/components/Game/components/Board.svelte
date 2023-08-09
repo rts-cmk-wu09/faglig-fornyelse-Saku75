a88eef32d9225e5539900d8b6f1f665e0f13afef
Lukas Mann <lukas@lvmann.io>
2023-08-09 17:24:31 +0200

    Added data binding to store from board

--- src/lib/components/Game/components/Board.svelte
+++ src/lib/components/Game/components/Board.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
+  import { roundNumber } from "$lib/utils/roundNumber";
+  import { gameStore } from "../gameStore";
+
+  // Calculate board aspect ratio
+  $: $gameStore.board.aspectRatio = roundNumber(
+    $gameStore.board.width / $gameStore.board.height,
+    {
+      direction: "up",
+      precision: 2,
+    }
+  );
+
+  // Check if aspect ratio has changed once
+  $: if ($gameStore.board.aspectRatio !== 1.78) {
+    $gameStore.board.aspectRatioChanged = true;
+  }
 </script>
 
 <div
+  class="relative mx-auto aspect-video max-h-screen outline outline-neutral-700"
+  bind:clientWidth={$gameStore.board.width}
+  bind:clientHeight={$gameStore.board.height}
 >
   <slot />
 </div>
